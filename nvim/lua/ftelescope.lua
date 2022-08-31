6c97908ac4035caa1172cdf28c03a2b3b4e5bacf
Gabriel Lara <gtlchaves@gmail.com>
2022-08-31 18:00:43 -0300

    Configures find_files to open in new tab

--- nvim/lua/ftelescope.lua
+++ nvim/lua/ftelescope.lua
@@ -1,16 +1,11 @@
--- local telescope = require('telescope')
--- local telescope = require('telescope').setup({layout_strategy='vertical',layout_config={width=0.5}})
-
--- require("telescope").setup({
---     defaults = {
---         layout_strategy = 'vertical',
---         layout_config = {height = 20, widht=30},
---     },
--- })
-
-require("telescope").setup({
-    defaults = {
-        layout_strategy = 'horizontal'
-        -- layout_config = {height = 20, widht=30},
+require("telescope").setup {
+  pickers = {
+    find_files = {
+      mappings = {
+        n = {
+          ["<CR>"] = "select_tab",
+        },
+      },
     },
-})
+  },
+}
